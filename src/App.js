9ee247c439347c3c5bc47f85439a8df495634519
Tosyanchyk <ddd.tosha@gmail.com>
2022-07-26 14:29:55 +0300

    add colors and fonts to settings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
+import s from './App.module.sass'
 function App() {
   return (<div className="App" >
-
+    <div className={s.container}>
+      container
+    </div>
   </div>
   );
 }
